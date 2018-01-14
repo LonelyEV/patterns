77c909a05ca61e1dbf6195e8c7c532aa2cc48260
屈志刚 <598700560@qq.com>
2018-01-14 13:02:39 +0800

    first commit!

---
+++ src/main/java/onem/quzhigang/patterns/App.java
@@ -0,0 +1,13 @@
+package onem.quzhigang.patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
